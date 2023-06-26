53d5419278f22f9b6a09e8173bff1568ba4a5818
xander <shuaibualexander@gmail.com>
2023-06-26 08:34:10 +0100

    implement pubsub layer for network

--- packages/canisters/src/data_collection/src/lib.rs
+++ packages/canisters/src/data_collection/src/lib.rs
@@ -1,6 +1,48 @@
-#[ic_cdk::query]
+// this canister is responsible for collecting data
+// and then publishing it to all the remittance canisters subscribed to it
+// it essentially Loads -> transforms -> Publishes data
+use candid::{CandidType, Principal};
+use ic_cdk_macros::*;
+use serde::Deserialize;
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+
+type SubscriberStore = BTreeMap<Principal, Subscriber>;
+thread_local! {
+    static SUBSCRIBERS: RefCell<SubscriberStore> = RefCell::default();
+}
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Counter {
+    topic: String,
+    value: u64,
+}
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Subscriber {
+    topic: String,
+}
+
+#[query]
 fn greet(name: String) -> String {
     format!("Hello data_collection canister, {}!", name)
 }
 
-    
+#[update]
+fn subscribe(subscriber: Subscriber) {
+    let subscriber_principal_id = ic_cdk::caller();
+    SUBSCRIBERS.with(|subscribers| {
+        subscribers
+            .borrow_mut()
+            .insert(subscriber_principal_id, subscriber)
+    });
+}
+
+#[update]
+async fn publish(counter: Counter) {
+    SUBSCRIBERS.with(|subscribers| {
+        for (k, v) in subscribers.borrow().iter() {
+            if v.topic == counter.topic {
+                let _call_result: Result<(), _> = ic_cdk::notify(*k, "update_count", (&counter,));
+            }
+        }
+    });
+}
--- packages/canisters/src/remittance/src/lib.rs
+++ packages/canisters/src/remittance/src/lib.rs
@@ -1,4 +1,47 @@
-#[ic_cdk::query]
+// this canister basically first subscribes to a data collection canister
+// then once the data collection cannister has some data
+// it publishes it over the pub sub layer
+// this canister then
+use candid::{CandidType, Principal};
+use ic_cdk_macros::*;
+use serde::Deserialize;
+use std::cell::Cell;
+
+
+thread_local! {
+    static COUNTER: Cell<u64> = Cell::new(0);
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Counter {
+    topic: String,
+    value: u64,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Subscriber {
+    topic: String,
+}
+
+#[query]
 fn request() -> String {
     format!("Signature_response")
+}
+
+#[update]
+async fn setup_subscribe(publisher_id: Principal, topic: String) {
+    let subscriber = Subscriber { topic };
+    let _call_result: Result<(), _> = ic_cdk::call(publisher_id, "subscribe", (subscriber,)).await;
+}
+
+#[update]
+fn update_count(counter: Counter) {
+    COUNTER.with(|c| {
+        c.set(c.get() + counter.value);
+    });
+}
+
+#[update]
+fn get_count() -> u64 {
+    return COUNTER.with(|c| c.get());
 }
