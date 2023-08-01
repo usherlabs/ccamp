e74e1fd00ba5440a8fc2a4e90a16778a3e2d3d90
xander <shuaibualexander@gmail.com>
2023-08-01 19:03:22 +0100

    include query method as external method

--- packages/canisters/src/protocol_data_collection/src/logstore.rs
+++ packages/canisters/src/protocol_data_collection/src/logstore.rs
@@ -1,4 +1,4 @@
-use ic_cdk_macros::{init, post_upgrade, query};
+use ic_cdk_macros::{init, post_upgrade, query, update};
 use std::sync::atomic::{AtomicU64, Ordering};
 
 const TIMER_INTERVAL_SEC: u64 = 60;
@@ -24,6 +24,7 @@ fn upgrade() {
 }
 // ----------------------------------- init and upgrade hooks
 
+#[update]
 pub fn query_logstore() {
     // dummy action, will be replaced with http call to logstore network
     COUNTER.with(|counter| counter.fetch_add(1, Ordering::Relaxed));
