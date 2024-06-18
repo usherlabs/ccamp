2af615b717580cb9e4f590e7fd914dc084c633dd
kim <gusgkr0117@naver.com>
2024-06-18 07:49:16 +0000

    use config.ts

--- packages/host-node/src/node.ts
+++ packages/host-node/src/node.ts
@@ -2,21 +2,22 @@ import { HttpAgent } from "@dfinity/agent";
 import { canisterId, createActor } from "@ccamp/canisters/src/declarations/ic_af/index.js";
 import { Secp256k1KeyIdentity } from "@dfinity/identity-secp256k1";
 import { createRequire } from "node:module";
+import { environment } from "./config";
 
 import * as fs from 'fs';
 
 export async function start() {
+	const env = environment.dev;
 	const tls_data = fs.readFileSync('./fixtures/twitter_proof.json');
 
 	// Completely insecure seed phrase. Do not use for any purpose other than testing.
 	// Resolves to "rwbxt-jvr66-qvpbz-2kbh3-u226q-w6djk-b45cp-66ewo-tpvng-thbkh-wae"
 	const seed = "test test test test test test test test test test test test";
-
 	const identity = Secp256k1KeyIdentity.fromSeedPhrase(seed);
 
 	// Require syntax is needed for JSON file imports
 	const require = createRequire(import.meta.url);
-	const localCanisterIds = require("@ccamp/canisters/.dfx/local/canister_ids.json");
+	const localCanisterIds = require(env.canisterIdPath);
 
 	// Use `process.env` if available provoded, or fall back to local
 	const effectiveCanisterId =
@@ -24,7 +25,7 @@ export async function start() {
 
 	const agent = new HttpAgent({
 	identity: identity,
-	host: "http://127.0.0.1:4943",
+	host: env.agentUrl,
 	fetch,
 	});
 
